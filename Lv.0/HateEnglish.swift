7c36362f0f23613376592376d3369940805bae55
zongbeen <hjb825@naver.com>
2023-03-28 23:38:45 +0900

    23/03/28

---
+++ Lv.0/HateEnglish.swift
@@ -0,0 +1,24 @@
+//
+//  HateEnglish.swift
+//  CodingTest
+//
+//  Created by 한종빈 on 2023/03/28.
+//
+
+import Foundation
+
+func solution(_ numbers:String) -> Int64 {
+    let result = numbers
+        .replacingOccurrences(of: "zero", with: "0")
+        .replacingOccurrences(of: "one", with: "1")
+        .replacingOccurrences(of: "two", with: "2")
+        .replacingOccurrences(of: "three", with: "3")
+        .replacingOccurrences(of: "four", with: "4")
+        .replacingOccurrences(of: "five", with: "5")
+        .replacingOccurrences(of: "six", with: "6")
+        .replacingOccurrences(of: "seven", with: "7")
+        .replacingOccurrences(of: "eight", with: "8")
+        .replacingOccurrences(of: "nine", with: "9")
+
+    return Int64(result)!
+}
