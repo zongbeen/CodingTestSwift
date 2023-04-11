cb21c12cdf18c7508945bb3b1918e4576411a0dc
zongbeen <hjb825@naver.com>
2023-04-11 23:30:33 +0900

    24/04/11

--- Lv.0/SliceArray.swift
+++ Lv.0/SliceArray.swift
@@ -26,3 +26,9 @@ func solution(_ my_str:String, _ n:Int) -> [String] {
     }
     return result
 }
+
+/*
+String 변수에 입력받은 문자열을 하나씩 더한다.
+String 변수의 길이가 n 이 되면 result 배열에 추가한 후 String 변수는 다시 초기화한다.
+반복문이 종료된 후 String 변수가 비어있지 않으면 result 배열에 String 변수의 값을 추가한다. (문자열의 길이가 n으로 나누어 떨어지지 않는 경우)
+*/
