1eadff7f8dead30d2ad5b227da4154bac2e42e38
zongbeen Han <hjb825@naver.com>
2025-02-26 20:24:23 +0900

    25.02.25

---
+++ Lv.2/Fibonacci.swift
@@ -0,0 +1,15 @@
+//
+//  Fibonacci.swift
+//  CodingTest
+//
+//  Created by 한종빈 on 2/25/25.
+//
+
+import Foundation
+
+func fi(_ num: Int) -> Int {
+    if num <= 1 {
+        return num
+    }
+    return fi(num - 1) + fi(num - 2)
+}
